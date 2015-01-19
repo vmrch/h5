9db4cc9296eeaaaea8953dca697eb2c105a08333
venkat <srik14sri@gmail.com>
2015-01-19 12:09:42 -0600

    multiplication js

---
+++ script.js
@@ -0,0 +1,8 @@
+ function AddTwoValues() {
+        alert("jhghjg");
+        var a, b, result;
+        a = parseInt(document.getElementById("txtA").value);
+        b = parseInt(document.getElementById("txtB").value);
+        result = a + b;
+        document.getElementById("txtResult").value = result;
+    } 
